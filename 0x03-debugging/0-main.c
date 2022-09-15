352e66d98623378533e39268c72efcb48f9efece
Michae202 <mikewachirabb495@gmail.com>
2022-09-15 14:38:47 -0700

    main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,20 +1,50 @@
-#include "main.h"
+nclude "main.h"
+
+
 
 /**
-*_isupper - checks if a  character is uppercase or not
-*@c: character to be tested
-*Return: 1 whether it is, 0 otherwise
-*/
+ *
+ * * main - tests function that prints if integer is positive or negative
+ *
+ * * Return: 0
+ *
+ * */
 
-int _isupper(int c)
+
+
+int main(void)
+
 {
-	if ((c >= 'A') && (c <= 'Z'))
-	{
-		return (1);
-	}
+
+	int i;
+
+
+
+	i = 0;
+
+	positive_or_negative(i);
+
+
 
 	return (0);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
