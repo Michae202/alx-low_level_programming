f3f3d9fdf3c3fb25e60a8ee39b956cadb2b51fdd
Michae202 <mikewachirabb495@gmail.com>
2022-09-21 14:26:36 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,34 @@
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+#include "main.h"                                                                                                                    
+                                                                                                                                     
+/**                                                                                                                                  
+ * strcat - Concatenates the string pointed to by @src, including the terminat                                                       
+ *          null byte, to the end of the string pointed to by @dest.                                                                 
+ * @dest: A pointer to the string to be concatenated upon.                                                                           
+ * @src: The source string to be appended to @dest.                                                                                  
+ *                                                                                                                                   
+ * Return: A pointer to the destination string @dest.                                                                                
+ */                                                                                                                                  
+char *strcat(char *dest, const char *src)                                                                                            
+{                                                                                                                                    
+        int index = 0, dest_len = 0;                                                                                                 
+                                                                                                                                     
+        while (dest[index++])                                                                                                        
+                dest_len++;                                                                                                          
+                                                                                                                                     
+        for (index = 0; src[index]; index++)                                                                                         
+                dest[dest_len++] = src[index];                                                                                       
+                                                                                                                                     
+        return (dest);                                                                                                               
+}
+
+
+
+
+
+
+
+
+
+
+
+
