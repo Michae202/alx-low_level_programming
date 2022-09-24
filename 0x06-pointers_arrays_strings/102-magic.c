d345b3b03e194d048f47bee74c5a3a2a21a251a0
Michae202 <mikewachirabb495@gmail.com>
2022-09-23 21:25:16 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 int main(void)
 {
@@ -20,7 +20,29 @@ int main(void)
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
-}                                                                                           
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                              
                                                                                                                        
                                                                                                            
                                                                                                                                
